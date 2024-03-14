b1e72106083bb1aad2083503563a957f42d813eb
tatiane <tati.guisso@gmail.com>
2024-03-13 22:49:54 -0300

    implementando quartoController

---
+++ src/main/java/com/grupo16/hackathon/usecase/CriarAlterarQuartoUseCase.java
@@ -0,0 +1,15 @@
+package com.grupo16.hackathon.usecase;
+
+import org.springframework.stereotype.Service;
+
+import com.grupo16.hackathon.domain.Quarto;
+
+@Service
+public class CriarAlterarQuartoUseCase {
+
+	public Long criar(Quarto quarto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
