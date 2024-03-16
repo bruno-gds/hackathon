a578a113af340a636b6387458a20b1c354d482dc
TatiGuisso <tati.guisso@gmail.com>
2024-03-15 21:36:35 -0300

    Iplementando reserva controller

--- src/main/java/com/grupo16/hackathon/gateway/controller/ReservaController.java
+++ src/main/java/com/grupo16/hackathon/gateway/controller/ReservaController.java
@@ -1,8 +1,14 @@
 package com.grupo16.hackathon.gateway.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.grupo16.hackathon.domain.Reserva;
+import com.grupo16.hackathon.gateway.controller.json.ReservaJson;
 import com.grupo16.hackathon.usecase.CriarReservaUseCase;
 
 import lombok.AllArgsConstructor;
@@ -16,17 +22,16 @@ public class ReservaController {
 
     private CriarReservaUseCase criarReservaUseCase;
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public Long criar(@RequestBody ReservaJson reservaJson) {
+        log.trace("Start reservaJson={}", reservaJson);
 
-//    @ResponseStatus(HttpStatus.CREATED)
-//    @PostMapping
-//    public Long criar(@Valid @RequestBody ClienteJson clienteJson) {
-//        log.trace("Start clienteJson={}", clienteJson);
-//
-//        Cliente cliente = clienteJson.mapearParaDomain();
-//        Long id = criarAlterarClienteUseCase.criar(cliente);
-//
-//        log.trace("End id={}", id);
-//        return id;
-//    }
+        Reserva reserva = reservaJson.mapearParaDomain();
+        Long id = criarReservaUseCase.criar(reserva);
+
+        log.trace("End id={}", id);
+        return id;
+    }
 
 }
